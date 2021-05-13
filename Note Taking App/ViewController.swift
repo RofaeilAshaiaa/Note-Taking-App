8908b29367f2b06e96dd30dc6427ed13e2cbca52
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2021-05-13 22:25:59 +0200

    feature: Add list of notes UI components and displaying a static	 list of data

--- Note Taking App/ViewController.swift
+++ Note Taking App/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var notesTable: UITableView!
+    var data: [String] = ["Item 1", "Item 2", "Item 3", "Item 4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        notesTable.dataSource = self
+        self.title = "Notes"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"cell")!
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 
 }
 
